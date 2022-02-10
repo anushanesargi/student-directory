5e32a15acb76ba862c2926b0303184c8edeea99b
Anusha Nesargi <anushanesargi@gmail.com>
2022-02-10 15:42:44 +0000

    Introduced user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,35 @@
 # an array of students
-student = [
-{ name: "Dr. Hannibal Lecter", cohort: :November },
-{ name: "Darth Vader", cohort: :November }, 
-{ name: "Nurse Ratched", cohort: :November },
-{ name: "Michael Corleone", cohort: :November },
-{ name: "Alex DeLarge", cohort: :November },
-{ name: "The Wicked Witch of the West", cohort: :November },
-{ name: "Terminator", cohort: :November },
-{ name: "Freddy Krueger", cohort: :November },
-{ name: "The Joker", cohort: :November },
-{ name: "Joffrey Baratheon", cohort: :November },
-{ name: "Norman Bates", cohort: :November }
-]
+# student = [
+# { name: "Dr. Hannibal Lecter", cohort: :November },
+# { name: "Darth Vader", cohort: :November }, 
+# { name: "Nurse Ratched", cohort: :November },
+# { name: "Michael Corleone", cohort: :November },
+# { name: "Alex DeLarge", cohort: :November },
+# { name: "The Wicked Witch of the West", cohort: :November },
+# { name: "Terminator", cohort: :November },
+# { name: "Freddy Krueger", cohort: :November },
+# { name: "The Joker", cohort: :November },
+# { name: "Joffrey Baratheon", cohort: :November },
+# { name: "Norman Bates", cohort: :November }
+# ]
+
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # create an empty array
+    students = []
+    name = gets.chomp
+    while !name.empty?
+        students << {name: name, cohort: :November}
+        if students.count == 1
+            puts "Now we have #{students.count} student"
+        else
+            puts "Now we have #{students.count} students"
+        end
+        name = gets.chomp
+    end
+    students
+end
 
 #print header
 def header
@@ -31,6 +49,7 @@ def footer(students) # takes an array
     puts "Overall, we have #{students.count} great students"
 end
 
+student = input_students
 header
 names(student)
 footer(student)
