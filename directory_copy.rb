2279c225c7a7d0a398bda83170652674c244a721
Anusha Nesargi <anushanesargi@gmail.com>
2022-02-11 14:47:19 +0000

    Step10, Refactoring the methods to not more than 10 lines

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,51 +1,47 @@
-def menu
-    students = []
-    loop do
-        # 1. print the menu
-        puts "Please select one of the following:"
-        puts "1. Input the students"
-        puts "2. Print the students"
-        puts "9. Exit program"
-        
-        # 2. read the input and save it into a variable
-        user_input = gets.chomp
+@students = []
 
-        # 3. Do what the user asked
-        case user_input
-        when "1"
-            students = input_students
-            
-        when "2"
-            header
-            names(students)
-            footer(students)
-        when "9"
-            break
-        else
-            puts "Please enter from the selection"
-        end
-        # 4. repeat from step 1
+def menu
+    loop do
+        puts "Please select one of the following:"
+        print_menu
+        process(gets.chomp)
     end
 end
 
+def print_menu
+    puts "1. Input the students"
+    puts "2. Print the students"
+    puts "9. Exit program"
+end
+
+def show_students
+    header
+    names(@students)
+    footer(@students)
+end
+
+def process(selection)
+    case selection
+    when "1"
+        @students = input_students    
+    when "2"
+        show_students
+    when "9"
+        exit
+    else
+        puts "Please enter from the selection"
+    end
+end
 
 def input_students
     puts "Please enter the names of the students and their cohort"
     puts "To finish, just type quit"
-    # create an empty array
-    students = []
-    # name_and_cohort = []
-    # name = name_and_cohort[0].capitalize
-    # cohort = name_and_cohort[1].strip.capitalize.to_sym
+
     name_and_cohort = gets.chomp.split(",")
     
     while name_and_cohort[0] != "Quit"
-        
-        while name_and_cohort.empty?
-            puts "Please type name and cohort separated by a comma"
-            name_and_cohort = gets.chomp.split(",")
-        end
-        
+        name_and_cohort = check_input(name_and_cohort)
+
         break if name_and_cohort[0] == "Quit"
 
         if !name_and_cohort.empty? && name_and_cohort[0] != "Quit"
@@ -53,35 +49,48 @@ def input_students
             cohort = name_and_cohort[1].strip.capitalize.to_sym
         end
         
-        
-        if cohort != :November && cohort[0] == "N"
-            cohort = :November
-        elsif cohort != :December && cohort[0] == "D"
-            cohort = :December
-        else 
-            cohort = :TBC
-        end
-        students << {name: name, cohort: cohort, hobby: :skiing, food: :upma}
-        puts "#{students}"
-        
-        if students.count == 1
-            puts "Now we have #{students.count} student"
-        else
-            puts "Now we have #{students.count} students"
-        end
-
+        @students << {name: name, cohort: typo(cohort), hobby: :skiing, food: :upma}
+        student_count(@students)
         name_and_cohort = gets.chomp.split(",")
     end
-    students
+
+    @students
 end
 
-#print header
+def typo(cohort)
+    if cohort == :November || cohort == :December
+        cohort
+    elsif cohort != :November && cohort[0] == "N"
+        cohort = :November
+    elsif cohort != :December && cohort[0] == "D"
+        cohort = :December
+    else 
+        cohort = :TBC
+    end
+end
+
+def student_count(students)
+    if students.count == 1
+        puts "Now we have #{students.count} student"
+    else
+        puts "Now we have #{students.count} students"
+    end
+end
+
+def check_input(name_and_cohort)
+    while name_and_cohort.empty?
+        puts "Please type name and cohort separated by a comma"
+        name_and_cohort = gets.chomp.split(",")
+    end
+    name_and_cohort
+end
+
 def header
     puts "The students of Villains Academy".center(80,'.')
     puts "-------------".center(80)
 end
 
-#print names
+
 def names(students) # takes an array
     puts students
     i = 0
