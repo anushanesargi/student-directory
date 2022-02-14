523b633585cadf1b89865bdd089a394881f7e644
Anusha Nesargi <anushanesargi@gmail.com>
2022-02-14 10:59:59 +0000

    Step11 Saving the students

--- directory_copy.rb
+++ directory_copy.rb
@@ -11,6 +11,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Print the students"
+    puts "3. Save the list to student.csv"
     puts "9. Exit program"
 end
 
@@ -28,6 +29,8 @@ def process(selection)
         show_students
     when "9"
         exit
+    when "3"
+        save_students
     else
         puts "Please enter from the selection"
     end
@@ -110,4 +113,13 @@ def footer(students) # takes an array
     puts "Overall, we have #{students.count} great students"
 end
 
-menu
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
+        menu
